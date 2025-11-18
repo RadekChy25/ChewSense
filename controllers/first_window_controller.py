2a06f7fcd11ebc277b5375c2e5f928b8adf79b8b
RadekChy25 <chylak.radoslaw@gmail.com>
2025-11-18 16:22:30 +0100

    navigation working

--- controllers/first_window_controller.py
+++ controllers/first_window_controller.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QWidget
 from widgets.first_window import Ui_Form
 from controllers.start_session_controller import Start_session_controller
 from controllers.create_user_form_controller import Create_user_form_controller
+from controllers.main_window_controller import Main_window_controller
 
 class First_window_controller(QWidget):
     def __init__(self):
@@ -10,14 +11,25 @@ class First_window_controller(QWidget):
         
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        
+        self.create_user_controller = Create_user_form_controller()
+        self.start_session_controller = Start_session_controller()
+        self.main_window_controller = Main_window_controller()
 
         self.ui.add_user_btn.clicked.connect(self.add_user)
         self.ui.add_session_btn.clicked.connect(self.add_session)
 
+        self.start_session_controller.ui.start_session_btn.clicked.connect(self.open_main_window)
+
     def add_user(self): 
-        self.create_user_widget = Create_user_form_controller()
-        self.create_user_widget.show()
+        self.create_user_controller.show()
 
     def add_session(self):
-        self.create_session_widget = Start_session_controller()
-        self.create_session_widget.show()
+        self.start_session_controller.show()
+
+    def open_main_window(self):
+        self.main_window_controller.show()
+        
+        if self.start_session_controller.isVisible() or self.isVisible():
+            self.start_session_controller.close()
+            self.close()
--- controllers/main_window_controller.py
+++ controllers/main_window_controller.py
@@ -7,3 +7,19 @@ class Main_window_controller(QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.home_btn.clicked.connect(lambda: self.nav_button_switching(self.ui.home_btn))
+        self.ui.diagram_btn.clicked.connect(lambda: self.nav_button_switching(self.ui.diagram_btn))
+        self.ui.data_btn.clicked.connect(lambda: self.nav_button_switching(self.ui.data_btn))
+        self.ui.download_btn.clicked.connect(lambda: self.nav_button_switching(self.ui.download_btn))
+
+    def nav_button_switching(self, button_name):
+        buttons = [
+            self.ui.home_btn,
+            self.ui.diagram_btn,
+            self.ui.data_btn,
+            self.ui.download_btn
+        ]
+        
+        for btn in buttons:
+            btn.setChecked(btn is button_name)
